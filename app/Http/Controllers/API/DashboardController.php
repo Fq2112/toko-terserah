ca984144e7592a47a1a9bf5f26ce279821aa7113
m1ku100 <ilham.puji100@gmail.com>
2020-10-26 19:49:42 +0700

    commit 14/10/2020 13:25 : - Product API ( filter search API )

--- app/Http/Controllers/API/DashboardController.php
+++ app/Http/Controllers/API/DashboardController.php
@@ -144,7 +144,7 @@ class DashboardController extends Controller
                     ]
                 ])->getBody()->getContents();
                 $response = json_decode($response);
-//                dd($response);
+                dd($response);
                 if($response['rajaongkir']['status']['code'] == 200) {
                     if($pesanan->kode_kurir != 'pos') {
                         $i=count($response['rajaongkir']['result']['manifest']);
