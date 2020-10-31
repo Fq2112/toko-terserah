594960b683460edad452b691adb27acb8471be8c
Fahmi Rizky <vreallyla@gmail.com>
2020-10-31 20:43:41 +0700

    comment notice close payment midtrans

--- resources/views/pages/webviews/snap-midtrans.blade.php
+++ resources/views/pages/webviews/snap-midtrans.blade.php
@@ -152,9 +152,9 @@
         }
     }
 
-    $(window).on('beforeunload', function () {
-        return "You have attempted to leave this page. Are you sure?";
-    });
+    // $(window).on('beforeunload', function () {
+    //     return "You have attempted to leave this page. Are you sure?";
+    // });
 </script>
 </body>
 </html>
