e2bd3b933f1b166b5118c37426384b07aaa2104b
vreallyla <vreallyla@gmail.com>
2020-12-11 16:01:15 +0700

    Update Isi Cara belanja

--- resources/views/faq/cara_belanja.blade.php
+++ resources/views/faq/cara_belanja.blade.php
@@ -7,6 +7,20 @@
     <title>Document</title>
 </head>
 <body>
-<i>hello</i>
+    <p>Kami melayani pembelian untuk seluruh wilayah Indonesia. Baik secara on-line maupun via telepon
+        atau e-mail. Kami akan selalu berusaha untuk memberikan pelayanan yang cepat dan praktis.</p>
+    <h3>Belanja Online Menggunakan Keranjang Belanja</h3>
+        <ul>
+            <li>Pilih produk yang ingin Anda beli dengan menekan tombol "Masukkan Keranjang" atau tekan tombol " Beli" (untuk membeli produk yang dipilih saja), Maka produk yang Anda pilih akan masuk ke dalam "Keranjang Belanja"</li>
+            <li>Setelah menekan tombol masukkan keranjang, maka akan muncul perintah untuk memasukkan jumlah quantity yang mau anda beli</li>
+            <li>Kemudian lakukan check out produk yang akan ada beli dengan cara melakukan check list di keranjang</li>
+            <li>Kemudian anda akan diarahkan ke halaman pembayaran</li>
+        </ul>
+    <h3>Belanja Via Telepon / Email</h3>
+        <ul>
+            <li>Kami juga melayani pembelian melalui telepon 0811-3191-081 (telp/Whatsapp)</li>
+            <li>Anda juga bisa berbelanja dan menghubungi kami melalui email info@tokoterserah.com</li>
+        </ul>
+
 </body>
 </html>
