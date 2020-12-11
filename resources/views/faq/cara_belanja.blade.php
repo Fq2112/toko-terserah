36df7430baf8c772411be150e33d644fae19ea59
vreallyla <vreallyla@gmail.com>
2020-12-11 16:03:49 +0700

    Edit Cara Belanja

--- resources/views/faq/cara_belanja.blade.php
+++ resources/views/faq/cara_belanja.blade.php
@@ -9,7 +9,7 @@
 <body>
     <p>Kami melayani pembelian untuk seluruh wilayah Indonesia. Baik secara on-line maupun via telepon
         atau e-mail. Kami akan selalu berusaha untuk memberikan pelayanan yang cepat dan praktis.</p>
-    <h3>Belanja Online Menggunakan Keranjang Belanja</h3>
+    <h3>Belanja Online</h3>
         <ul>
             <li>Pilih produk yang ingin Anda beli dengan menekan tombol "Masukkan Keranjang" atau tekan tombol " Beli" (untuk membeli produk yang dipilih saja), Maka produk yang Anda pilih akan masuk ke dalam "Keranjang Belanja"</li>
             <li>Setelah menekan tombol masukkan keranjang, maka akan muncul perintah untuk memasukkan jumlah quantity yang mau anda beli</li>
