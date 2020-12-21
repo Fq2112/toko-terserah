67ebd7f3437b576d39f24a09ef10baeb2d47a8f0
vreallyla <vreallyla@gmail.com>
2020-12-21 21:24:42 +0700

    faq-ganti password

--- resources/views/faq/cara_ganti_password.blade.php
+++ resources/views/faq/cara_ganti_password.blade.php
@@ -7,6 +7,16 @@
     <title>Document</title>
 </head>
 <body>
-<i>Cara GantiPassword</i>
+    <font size="4">Pertama tekan tombol profile/akun :</font><br><br>
+    <img class="tengah" src="{{asset('images/faq/gantipassword_1.jpg')}}" width="250" alt=""><br><br>
+    <font size="4">Selanjutnya tekan foto profile anda :</font><br><br>
+    <img class="tengah" src="{{asset('images/faq/gantipassword_2.jpg')}}" width="250" alt=""><br><br>
+    <img class="tengah" src="{{asset('images/faq/gantipassword_3.jpg')}}" width="250" alt=""><br><br>
+    <font size="4">Silahkan anda geser kebawah, sampai muncul seperti ini :</font><br><br>
+    <img class="tengah" src="{{asset('images/faq/gantipassword_4.jpg')}}" width="250" alt=""><br><br>
+    <font size="4">Silahkan anda isi password lama anda dan isi password baru anda, kemudian tekan tombol ubah password.</font><br><br>
+
+
+
 </body>
 </html>
