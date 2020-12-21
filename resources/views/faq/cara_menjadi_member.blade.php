e2965a4b6d7147a264bba990cdaa76645f055b0e
vreallyla <vreallyla@gmail.com>
2020-12-21 22:20:03 +0700

    faq-membercard

--- resources/views/faq/cara_menjadi_member.blade.php
+++ resources/views/faq/cara_menjadi_member.blade.php
@@ -6,7 +6,18 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
+<style>
+    img.tengah {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+}
+</style>
 <body>
-<i>Cara MenjadiMember</i>
+    <font size="4">Dengan anda melakukan registrasi di toko online kami makan saat itu juga anda menjadi anggota member kami, untuk melakukan verifikasi member toko fisik kami silahkan anda tunjukkan Kartu Member yang ada di aplikasi anda, dengan cara sebagai berikut :</font><br><br>
+    <font size="4">Masuk halaman akun, kemudian pilih Kartu Member :</font><br><br>
+    <img class="tengah" src="{{asset('images/faq/member_1.jpg')}}" width="250" alt=""><br><br>
+    <img class="tengah" src="{{asset('images/faq/member_2.jpg')}}" width="250" alt=""><br><br>
+
 </body>
 </html>
