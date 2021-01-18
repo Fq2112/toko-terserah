260c9e2d49fb04360d37c851d63e0df1460d6d9c
Fiqy Ainuzzaqy <fiqy_a@icloud.com>
2021-01-18 14:43:38 +0700

    nicescroll kategori

--- resources/views/pages/main/beranda.blade.php
+++ resources/views/pages/main/beranda.blade.php
@@ -46,9 +46,9 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-3">
-                    <div class="shop-sidebar use-nicescroll" style="max-height: 500px;overflow: auto;">
+                    <div class="shop-sidebar">
                         <button class="btn-toggle"><i class="fa fa-reorder"></i></button>
-                        <ul class="shop-category">
+                        <ul class="shop-category use-nicescroll" style="max-height: 500px;overflow: auto;>
                             @foreach($kategori as $kat)
                                 <li class="category-dropdown">
                                     <a href="javascript:void(0)">{{$kat->nama}} <i class="fa fa-caret-right"></i></a>
